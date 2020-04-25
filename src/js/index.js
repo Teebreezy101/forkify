dcce69c8733e9daa993352125ff8643214979550
Teebreezy101 <tammy.batubo@gmail.com>
2020-04-25 01:20:05 +0100

    made the likes btn and controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/recipe';
+import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {elements, renderLoader, clearLoader} from './views/base';
 /** Global state of the app
  * - Search object
@@ -11,6 +14,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
  * - Liked recipes
  */
 const state = {};
+window.state = state;
 
 
 /**
@@ -81,7 +85,6 @@ const controlRecipe = async() => {
         try {
             // get recipe data and parse ingredients
             await state.recipe.getRecipe();
-            console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
 
             // calculate servings and time
@@ -102,6 +105,72 @@ const controlRecipe = async() => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+// LIST CONTROLLER
+const controlList = () => {
+    //create a new list if there is none yet
+    if(!state.list) state.list = new List();
+
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Handle the delete button
+    if(e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+
+        //Delete from UI
+        listView.deleteItem(id);
+
+        //Handle the count update
+    }else if(e.target.matches('.shopping__count--value')) {
+        const val = pardeFloat(e.target.value);
+        state.list.updateCount(id, val);
+    }
+});
+
+
+//LIKE CONTROLLER
+
+const controlLike = () => {
+    if(!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+    //User has not yet liked current recipe
+    if(!state.likes.isLiked(currentID)) {
+        //Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+            )
+        //Toggle the like button
+
+        //Add like to UI list
+        console.log(state.likes);
+        
+        //User has liked current recipe
+    }else {
+        //Remove like from the state
+        state.likes.deleteLike(currentID);
+        //Toggle the like button
+        
+        //Remove like from UI list
+        console.log(state.likes);
+
+    }
+};
+
+
+
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -115,6 +184,12 @@ elements.recipe.addEventListener('click', e => {
         // Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // Add ingredients to shopping list
+        controlList();
+    }else if(e.target.matches('.recipe__love, .recipe__love *')) {
+        controlLike();
     }
-    console.log(state.recipe);
 });
+
+window.l = new List();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,9 @@
 import {elements} from './base';
 import {Fraction } from 'fractional';
 
-
+export const clearRecipe = () => {
+    elements.recipe.innerHTML = '';
+};
 
 
 const formatCount = count => {
@@ -85,7 +87,7 @@ export const renderRecipe = recipe => {
                     ${recipe.ingredients.map(el => createIngredient(el)).join('')}
                 </ul>
 
-                <button class="btn-small recipe__btn">
+                <button class="btn-small recipe__btn recipe__btn--add">
                     <svg class="search__icon">
                         <use href="img/icons.svg#icon-shopping-cart"></use>
                     </svg>
